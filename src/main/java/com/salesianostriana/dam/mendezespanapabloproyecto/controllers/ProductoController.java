c64a5f7ec3e9a1fd6bb3b775f2ebd850c44bd4d2
Pablo <mendez.espab24@triana.salesianos.edu>
2024-05-06 12:25:12 +0200

    Navbar y footer realizados

--- src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/controllers/ProductoController.java
+++ src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/controllers/ProductoController.java
@@ -1,27 +1,33 @@
 package com.salesianostriana.dam.mendezespanapabloproyecto.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.salesianostriana.dam.mendezespanapabloproyecto.model.Producto;;
+import com.salesianostriana.dam.mendezespanapabloproyecto.model.Producto;
+import com.salesianostriana.dam.mendezespanapabloproyecto.services.ProductoService;;
 
 @Controller
 public class ProductoController {
 	
-	@GetMapping("/producto")
-	public String showForm(Model model) {
+	private ProductoService productoService;
+	
+	@GetMapping("/producto/nuevo")
+	public String showNewProductForm(Model model) {
 		Producto producto = new Producto();
 		model.addAttribute("productoForm", producto);
 		return "formularioRegistroProducto";
 	}
 	
-	@PostMapping("/addProducto")
+	@PostMapping("/producto/nuevo/submit")
 	public String submit (@ModelAttribute("productoForm") Producto producto, Model model) {
 		model.addAttribute("producto", producto);
-		return "view";
+		return "redirect:/portada";
 	}
 	
 }
