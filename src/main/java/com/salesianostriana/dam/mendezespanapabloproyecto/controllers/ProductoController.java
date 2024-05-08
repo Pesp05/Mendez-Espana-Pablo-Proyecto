03a3169eef4a81805ccc1fc1a22aba5c79d6b54c
Pablo <mendez.espab24@triana.salesianos.edu>
2024-05-08 13:11:19 +0200

    Registrar productos implemenado

--- src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/controllers/ProductoController.java
+++ src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/controllers/ProductoController.java
@@ -2,6 +2,7 @@ package com.salesianostriana.dam.mendezespanapabloproyecto.controllers;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianostriana.dam.mendezespanapabloproyecto.model.Producto;
-import com.salesianostriana.dam.mendezespanapabloproyecto.services.ProductoService;;
+import com.salesianostriana.dam.mendezespanapabloproyecto.services.ProductoService;
 
 @Controller
 public class ProductoController {
 	
+	@Autowired
 	private ProductoService productoService;
 	
 	@GetMapping("/producto/nuevo")
@@ -25,9 +27,9 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/producto/nuevo/submit")
-	public String submitNewProductoForm (@ModelAttribute("productoForm") Producto producto, Model model) {
+	public String submitNewProductoForm (@ModelAttribute("producto") Producto producto) {
 		productoService.save(producto);
-		return "redirect:admin/producto/nuevo";
+		return "admin/formularioRegistroProducto";
 	}
 	
 	@GetMapping("/producto/editar/{id}")
@@ -42,7 +44,7 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/producto/editar/submit")
-	public String submitEditProductForm(@ModelAttribute("producto") Producto producto, Model model) {
+	public String submitEditProductForm(@ModelAttribute("producto") Producto producto) {
 		productoService.edit(producto);
 		return "redirect:/producto/nuevo";
 	}
