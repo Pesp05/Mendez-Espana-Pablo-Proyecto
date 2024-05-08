b9c5760c316b447435bcdd76c38ca69e6fe5ee5c
Pablo <mendez.espab24@triana.salesianos.edu>
2024-05-08 09:08:50 +0200

    Formulario HTML implementado y conectado mediante thymeleaf

--- src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/model/Producto.java
+++ src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/model/Producto.java
@@ -22,55 +22,6 @@ public class Producto {
 	private double precioFabrica;
 	private String imagen;
 	private String marca;
-	private char size;
-	private String color;
 	private String descripcion;
-	private double valoracion;
-	private boolean disponibilidad;
-	
-	//Other Constructors
-	public Producto(Long id, String nombre, double precioVenta, double precioFabrica, String imagen, String marca,
-			char size, String color, String descripcion, boolean disponibilidad) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.precioVenta = precioVenta;
-		this.precioFabrica = precioFabrica;
-		this.imagen = imagen;
-		this.marca = marca;
-		this.size = size;
-		this.color = color;
-		this.descripcion = descripcion;
-		this.disponibilidad = disponibilidad;
-	}
-
-	public Producto(Long id, String nombre, double precioVenta, double precioFabrica, String imagen, String marca,
-			char size, String color, String descripcion) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.precioVenta = precioVenta;
-		this.precioFabrica = precioFabrica;
-		this.imagen = imagen;
-		this.marca = marca;
-		this.size = size;
-		this.color = color;
-		this.descripcion = descripcion;
-	}
-
-	public Producto(Long id, String nombre, double precioVenta, double precioFabrica, String imagen, String marca,
-			char size, String color, String descripcion, double valoracion) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.precioVenta = precioVenta;
-		this.precioFabrica = precioFabrica;
-		this.imagen = imagen;
-		this.marca = marca;
-		this.size = size;
-		this.color = color;
-		this.descripcion = descripcion;
-		this.valoracion = valoracion;
-	}
 	
 }
