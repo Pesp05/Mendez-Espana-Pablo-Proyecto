46f93db83fb86db3e5b8bb4bfb04ff2d2297d6d2
Pablo <mendez.espab24@triana.salesianos.edu>
2024-05-16 14:29:42 +0200

    Avances en vista de productos 16/05/2024

--- src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/services/ProductoService.java
+++ src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/services/ProductoService.java
@@ -1,11 +1,19 @@
 package com.salesianostriana.dam.mendezespanapabloproyecto.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.salesianostriana.dam.mendezespanapabloproyecto.model.Categoria;
 import com.salesianostriana.dam.mendezespanapabloproyecto.model.Producto;
 import com.salesianostriana.dam.mendezespanapabloproyecto.repositories.ProductoRepository;
 import com.salesianostriana.dam.mendezespanapabloproyecto.services.base.BaseServiceImpl;
 
 @Service
 public class ProductoService extends BaseServiceImpl<Producto, Long, ProductoRepository>{
+	
+	public List<Producto> buscarPorCategoria(Categoria categoria){
+		return repository.productosPorCategoria(categoria);
+	}
 	
 }
