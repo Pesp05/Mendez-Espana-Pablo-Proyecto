5e634dc0054c41dfd326c690ac724bf43ad555e0
Pesp05 <mendez.espab24@triana.salesianos.edu>
2024-05-20 21:59:24 +0200

    Edición de perfil implementada

--- src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/controllers/UsuarioController.java
+++ src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/controllers/UsuarioController.java
@@ -1,14 +1,11 @@
 package com.salesianostriana.dam.mendezespanapabloproyecto.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,12 +18,7 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
-	/*
-	@GetMapping("/{id}")
-	public String showUserAccount(@PathVariable("id") long id, Model model) {
-		
-	}
-	*/
+
 	@GetMapping("/nuevo")
 	public String showNewUserForm(Model model) {
 		Usuario usuario = new Usuario();
@@ -40,22 +32,16 @@ public class UsuarioController {
 		return "redirect:/login";
 	}
 	
-	@GetMapping("/editar/{id}")
-	public String editUserById(@PathVariable("id") long id, Model model) {
-		Optional<Usuario> aEditar = usuarioService.findById(id);
-		if(aEditar.isPresent()) {
-			Usuario usuario = aEditar.get();
-			model.addAttribute("usuario", usuario);
-			return "formularioRegistroUsuario";
-		} else {
-			return "redirect:/portada";
-		}
+	@GetMapping("/editar")
+	public String editUser(@AuthenticationPrincipal Usuario usuario, Model model) {
+		model.addAttribute("usuario", usuario);
+		return "formularioRegistroUsuario";
 	}
 	
 	@PostMapping("/editar/submit")
 	public String submitEditUserById(@ModelAttribute("usuario") Usuario usuario) {
 		usuarioService.edit(usuario);
-		return "redirect:/portada";
+		return "redirect:/usuario/perfil";
 	}
 	
 	@GetMapping("/perfil")
