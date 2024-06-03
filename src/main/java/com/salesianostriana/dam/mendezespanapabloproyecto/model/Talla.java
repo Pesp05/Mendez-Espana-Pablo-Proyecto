935ebfcd6dd868e247bbf777db5bf904a40bb559
Pablo <mendez.espab24@triana.salesianos.edu>
2024-06-03 12:28:29 +0200

    helper methods added and associations changed properly

--- src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/model/Talla.java
+++ src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/model/Talla.java
@@ -1,12 +1,19 @@
 package com.salesianostriana.dam.mendezespanapabloproyecto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor
@@ -18,5 +25,11 @@ public class Talla {
 	private Long id;
 	
 	private String nombre;
+	
+	@ManyToMany(mappedBy="tallas", fetch = FetchType.EAGER)
+	@Builder.Default
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
+	private List<Producto> productos = new ArrayList<>();
 	
 }
