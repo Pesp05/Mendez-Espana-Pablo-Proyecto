e9d79a98d30e499f9cfe2e11f310ceeb62e30ea2
Pesp05 <mendez.espab24@triana.salesianos.edu>
2024-06-19 17:49:51 +0200

    añadida vista de usuario y ventas de cada uno

--- src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/controllers/AdminController.java
+++ src/main/java/com/salesianostriana/dam/mendezespanapabloproyecto/controllers/AdminController.java
@@ -1,15 +1,24 @@
 package com.salesianostriana.dam.mendezespanapabloproyecto.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.salesianostriana.dam.mendezespanapabloproyecto.model.LineaVenta;
+import com.salesianostriana.dam.mendezespanapabloproyecto.model.Usuario;
+import com.salesianostriana.dam.mendezespanapabloproyecto.model.Venta;
 import com.salesianostriana.dam.mendezespanapabloproyecto.services.ColorService;
 import com.salesianostriana.dam.mendezespanapabloproyecto.services.MarcaService;
 import com.salesianostriana.dam.mendezespanapabloproyecto.services.ProductoService;
 import com.salesianostriana.dam.mendezespanapabloproyecto.services.TallaService;
+import com.salesianostriana.dam.mendezespanapabloproyecto.services.UsuarioService;
+import com.salesianostriana.dam.mendezespanapabloproyecto.services.VentaService;
 
 @Controller
 @RequestMapping("/admin")
@@ -26,6 +35,12 @@ public class AdminController {
 	
 	@Autowired
 	private ColorService colorService;
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@Autowired
+	private VentaService ventaService;
 
 	@GetMapping("/menu")
 	public String showAdminMenu() {
@@ -44,6 +59,43 @@ public class AdminController {
 		model.addAttribute("listaTallasAdmin", tallaService.findAll());
 		model.addAttribute("listaColoresAdmin", colorService.findAll());
 		return "admin/listaAdminVariantes";
+	}
+	
+	@GetMapping("/lista/users")
+	public String showAllUsers(Model model) {
+		List<Usuario> listaUsuarios = usuarioService.findAll();
+		model.addAttribute("listaUsuarios", listaUsuarios);
+		return "admin/listaUsuariosAdmin";
+	}
+	
+	@GetMapping("/ventas/user/{id}")
+	public String showVentasFromUser(@PathVariable("id") Long id, Model model) {
+		Optional<Usuario> optUser = usuarioService.findById(id);
+		if(optUser.isPresent()) {
+			Usuario user = optUser.get();
+			List<Venta> listaVentasUser = user.getVentas();
+			model.addAttribute("listaVentasUser", listaVentasUser);
+			model.addAttribute("usuario", user);
+			return "admin/ventasUserAdmin";
+		} else {
+			throw new IllegalArgumentException("No se encuentra el usuario");
+		}
+	}
+	
+	@GetMapping("/venta/ver/detalles/{id}")
+	public String showDetailedVenta(@PathVariable("id") Long id, Model model) {
+		Optional<Venta> optVenta = ventaService.findById(id);
+		if(optVenta.isPresent()) {
+			Venta venta = optVenta.get();
+			Usuario user = venta.getUsuario();
+			List<LineaVenta> listaLineasVenta = venta.getListaLineasVenta();
+			model.addAttribute("venta", venta);
+			model.addAttribute("usuario", user);
+			model.addAttribute("listaLineasVenta", listaLineasVenta);
+			return "admin/detallesVentaAdmin";
+		} else {
+			throw new IllegalArgumentException("no se encuentra la venta");
+		}
 	}
 	
 }
